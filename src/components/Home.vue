bdd3c5c2f9b60b545bc5a20c47b3f62e7ced9c6d
matteo-cota <matteocota@outlook.it>
2024-11-19 18:15:00 +0100

    added component

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <h1>Progetti</h1>
+      <ul>
+        <li v-for="project in projects" :key="project.id">
+          <router-link :to="{ name: 'project-detail', params: { id: project.id }}">
+            {{ project.name }}
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        projects: [],
+      };
+    },
+    created() {
+      axios.get('http://localhost:8000/api/projects')
+        .then(response => {
+          this.projects = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+  };
+  </script>
+  
---
+++ src/components/ProjectDetail.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <h1>{{ project.name }}</h1>
+      <p>{{ project.description }}</p>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        project: {},
+      };
+    },
+    created() {
+      const id = this.$route.params.id;
+      axios.get(`http://localhost:8000/api/projects/${id}`)
+        .then(response => {
+          this.project = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+  };
+  </script>
+  
